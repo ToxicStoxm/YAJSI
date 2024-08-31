a9903b95afcf6ca55d394b912b84cd02156f5215
ToxicStoxm <dominik0815oberlechner@gmail.com>
2024-08-31 22:55:06 +0200

    Fix module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
-module YAJSI.main {
+module YAJSI {
     requires static lombok;
     requires org.jetbrains.annotations;
     requires org.yaml.snakeyaml;
     requires java.logging;
     exports com.toxicstoxm.YAJSI;
+    exports com.toxicstoxm.YAJSI.yaml;
 }
