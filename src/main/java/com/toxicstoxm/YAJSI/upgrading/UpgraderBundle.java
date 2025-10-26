3007ea980713157bc535868e780f808287863836
Dominik <dominik@crafti-servi.com>
2025-10-27 00:28:50 +0100

    UpgraderBundle concept

---
+++ src/main/java/com/toxicstoxm/YAJSI/upgrading/UpgraderBundle.java
@@ -0,0 +1,12 @@
+package com.toxicstoxm.YAJSI.upgrading;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface UpgraderBundle {
+    Class<?> upgraderBundle();
+}
