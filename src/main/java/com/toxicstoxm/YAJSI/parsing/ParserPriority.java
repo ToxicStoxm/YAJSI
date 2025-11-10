87cfaa0cc0b06ab8318977d993e96e2cb156f768
Dominik <dominik@crafti-servi.com>
2025-11-10 23:36:57 +0100

    Removed alternative parsing concept

--- src/main/java/com/toxicstoxm/YAJSI/parsing/ParserPriority.java
+++
@@ -1,5 +0,0 @@
-package com.toxicstoxm.YAJSI.parsing;
-
-public @interface ParserPriority {
-    int priority() default 0;
-}
--- src/main/java/com/toxicstoxm/YAJSI/parsing/YAMLGetter.java
+++
@@ -1,5 +0,0 @@
-package com.toxicstoxm.YAJSI.parsing;
-
-public interface YAMLGetter<P1, P2, R> {
-    R get(P1 param1, P2 param2);
-}
