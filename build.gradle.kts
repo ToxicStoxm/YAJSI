beaf02da6851cf22b8f11e42638cce9994bf6f45
Dominik <dominik@crafti-servi.com>
2026-02-11 16:04:47 +0100

    Move dependency specifications to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java-library")
-    id("com.vanniktech.maven.publish") version "0.36.0"
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
 group = "com.toxicstoxm"
@@ -11,31 +11,24 @@ repositories {
     mavenLocal()
 }
 
-val lombokVersion = "1.18.42"
-val jetbrainsAnnotationsVersion = "26.0.2-1"
-val junitVersion = "6.0.2"
-val classgraphVersion = "4.8.184"
-val stormYAMLVersion = "1.0.0"
-
 dependencies {
-    implementation("org.jetbrains:annotations:$jetbrainsAnnotationsVersion")
-    annotationProcessor("org.jetbrains:annotations:$jetbrainsAnnotationsVersion")
+    implementation(libs.jetbrains.annotations)
+    annotationProcessor(libs.jetbrains.annotations)
 
-    implementation("org.projectlombok:lombok:$lombokVersion")
-    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    implementation(libs.lombok)
+    annotationProcessor(libs.lombok)
 
-    implementation("com.toxicstoxm:StormYAML:$stormYAMLVersion")
+    implementation(libs.stormyaml)
+    implementation(libs.classgraph)
 
-    implementation("io.github.classgraph:classgraph:$classgraphVersion")
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
 
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:$junitVersion")
+    testImplementation(libs.lombok)
+    testAnnotationProcessor(libs.lombok)
 
-    testImplementation("org.projectlombok:lombok:$lombokVersion")
-    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
-
-    testImplementation("org.jetbrains:annotations:$jetbrainsAnnotationsVersion")
-    testAnnotationProcessor("org.jetbrains:annotations:$jetbrainsAnnotationsVersion")
+    testImplementation(libs.jetbrains.annotations)
+    testAnnotationProcessor(libs.jetbrains.annotations)
 }
 
 java {
